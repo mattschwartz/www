66647f28d2d2e1d7bc947af503edec19ef5ee0b7
Matt Schwartz <schwamat@gmail.com>
2015-11-27 21:05:23 -0600

    Cleaning up sq blog

--- stonequest/js/blog.js
+++ stonequest/js/blog.js
@@ -1,16 +1,6 @@
 $(document).ready(function () {
     var posts = [
         {
-            title: 'Story Time',
-            postedOn: 'November 21, 2015',
-            href: 'posts/story-time.html',
-            tags: [
-                'stonequest',
-                'java',
-                'story'
-            ]
-        },
-        {
             title: 'Never Satisfied',
             postedOn: 'November 17, 2015',
             href: 'posts/never-satisfied.html',
--- stonequest/js/main.js
+++ stonequest/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     autoPlay : 3000,
     stopOnHover : true,
     navigation:true,
-    paginationSpeed : 1000,
+    paginationSpeed : 5000,
     goToFirstSpeed : 2000,
     singleItem : true,
     autoHeight : true,
